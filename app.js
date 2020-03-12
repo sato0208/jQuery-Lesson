28ae848c1f779eb70fc44316cc3b9169e2cc41bf
里千晴 <satochiharu@satonoMacBook-Pro.local>
2020-03-12 17:09:28 +0900

    [finish]6-7

--- app.js
+++ app.js
@@ -34,13 +34,93 @@
 // $(function(){
 // 	$('.box1').hide();
 // });
+// 上から下にスライドダウンした後、色を青にして高さ、幅を変える。そしてスライドアップする
+// $(function(){
+// 	$('.box1').slideDown(function(){
+// 		$('.box1').css({
+// 			'background-color': '#0000FF',
+// 			'height':'100px',
+// 			'width':'200px'
+// 		}).slideUp();
+// 	});
+// });
 
+// マウスオーバーイベントを設定する
+// マウスアウトイベントを設定する
+// $(’.セレクタ名’)。イベント名（function(){イベント発生時に行われる処理});
+// $(function(){
+// 	$('.box1').mouseover(function(){
+// 		$('.box1').css({'background-color': '#0000FF'});
+// 	});
+
+// 	$('.box1').mouseout(function(){
+// 		$('.box1').css({'background-color': '#FF0000'});
+// 	});
+// });
+
+// class属性を追加する・解除する
+// $(function(){
+// 	$('.box1').mouseover(function(){
+// 		// 対象のHTML要素にclass属性を追加します
+// 		$('.box1').addClass('box1-ext');
+// 	});
+// 	$('.box1').mouseout(function(){
+// 		// 対象のHTML要素にclass属性を解除します
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
+// マウスクリックイベントの記述
 $(function(){
-	$('.box1').slideDown(function(){
-	$('.box1').css({
-		'background-color': '#0000FF',
-		'height':'100px',
-		'width':'200px'
-		}).slideUp();
+	$('.box1').on('click',function(){
+		$('.box1').addClass('box1-ext');
+	});
+
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
+	});
 });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
